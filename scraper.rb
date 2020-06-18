c664810a593caba76f54de061d725eb9c4a7dbdb
Pat McKenna <pat@mckenna.rocks>
2020-06-18 17:54:34 +1000

    Save some data to a file to avoid re-fetching
    I've got the logic down for fetching all my albums and all of the
    collectors who also share those albums. Now I wanna start fetching other
    albums from those collectors, and I wanna be able to do it without
    having to re-fetch that first lot of data every time.
    
    Not sure I should be committing this, but it's all publicly-available
    information anyway so 🤷‍♂️

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,7 @@
 require "scraperwiki"
 require "mechanize"
 require "faraday"
+require_relative "saved_data"
 
 agent = Mechanize.new
 
@@ -8,111 +9,89 @@ username = "blimpage"
 $fan_id = 11254
 collection_url = "https://bandcamp.com/#{username}"
 
-$all_albums = []
+if $saved_albums
+  $all_albums = $saved_albums
+else
+  $all_albums = []
 
-# Read in a page
-page = agent.get(collection_url)
+  # Read in a page
+  page = agent.get(collection_url)
 
-puts "grabbing initial albums"
+  puts "grabbing initial albums"
 
-# Find something on the page using css selectors
-album_elements = page.search('.collection-item-container')
+  # Find something on the page using css selectors
+  album_elements = page.search('.collection-item-container')
 
-initial_albums = album_elements.map do |album_element|
-  link_element = album_element.search(".item-link").first
+  initial_albums = album_elements.map do |album_element|
+    link_element = album_element.search(".item-link").first
 
-  {
-    tralbum_id: album_element["data-tralbumid"],
-    tralbum_type: album_element["data-tralbumtype"],
-    title: album_element.search(".collection-item-title").first.inner_text.gsub("(gift given)", "").strip,
-    artist: album_element.search(".collection-item-artist").first.inner_text.strip.gsub(/\Aby /, ""),
-    url: agent.agent.resolve(link_element["href"]).to_s,
-  }
-end
-
-$all_albums += initial_albums
-
-# From here we need to grab further albums by hitting Bandcamp's JSON API.
-# Each request needs a token from the previous request, and we can grab our
-# first "last token" out of the DOM here.
-last_token = album_elements.last["data-token"]
-
-more_albums_available = true
-
-while more_albums_available do
-  puts "grabbing more albums via JSON API"
-
-  response = Faraday.post(
-    "https://bandcamp.com/api/fancollection/1/collection_items",
-    { fan_id: $fan_id, older_than_token: last_token, count: 100}.to_json,
-    "Content-Type" => "application/json"
-  )
-
-  parsed_response = JSON.parse(response.body)
-
-  next_albums = parsed_response["items"].map do |item|
     {
-      tralbum_id: item["tralbum_id"],
-      tralbum_type: item["tralbum_type"],
-      title: item["item_title"],
-      artist: item["band_name"],
-      url: item["item_url"],
+      tralbum_id: album_element["data-tralbumid"],
+      tralbum_type: album_element["data-tralbumtype"],
+      title: album_element.search(".collection-item-title").first.inner_text.gsub("(gift given)", "").strip,
+      artist: album_element.search(".collection-item-artist").first.inner_text.strip.gsub(/\Aby /, ""),
+      url: agent.agent.resolve(link_element["href"]).to_s,
     }
   end
 
-  $all_albums += next_albums
-  more_albums_available = parsed_response["more_available"]
-  last_token = parsed_response["last_token"]
+  $all_albums += initial_albums
+
+  # From here we need to grab further albums by hitting Bandcamp's JSON API.
+  # Each request needs a token from the previous request, and we can grab our
+  # first "last token" out of the DOM here.
+  last_token = album_elements.last["data-token"]
+
+  more_albums_available = true
+
+  while more_albums_available do
+    puts "grabbing more albums via JSON API"
+
+    response = Faraday.post(
+      "https://bandcamp.com/api/fancollection/1/collection_items",
+      { fan_id: $fan_id, older_than_token: last_token, count: 100}.to_json,
+      "Content-Type" => "application/json"
+    )
+
+    parsed_response = JSON.parse(response.body)
+
+    next_albums = parsed_response["items"].map do |item|
+      {
+        tralbum_id: item["tralbum_id"],
+        tralbum_type: item["tralbum_type"],
+        title: item["item_title"],
+        artist: item["band_name"],
+        url: item["item_url"],
+      }
+    end
+
+    $all_albums += next_albums
+    more_albums_available = parsed_response["more_available"]
+    last_token = parsed_response["last_token"]
+  end
 end
 
 puts "no more albums, all done!"
 
 puts "#{$all_albums.count} albums total."
 
-$all_collectors = {}
+if $saved_collectors
+  $all_collectors = $saved_collectors
+else
+  $all_collectors = {}
 
-def get_collectors(tralbum_id:, tralbum_type:)
-  puts "getting initial collectors for album #{tralbum_id}."
-  collector_count_for_album = 0
+  def get_collectors(tralbum_id:, tralbum_type:)
+    puts "getting initial collectors for album #{tralbum_id}."
+    collector_count_for_album = 0
 
-  initial_response = Faraday.post(
-    "https://bandcamp.com/api/tralbumcollectors/2/initial",
-    { tralbum_type: tralbum_type, tralbum_id: tralbum_id, reviews_count: 0, thumbs_count: 500, exclude_fan_ids: [$fan_id] }.to_json,
-    "Content-Type" => "application/json"
-  )
-
-  parsed_initial_response = JSON.parse(initial_response.body)
-
-  parsed_initial_response["thumbs"].each do |collector|
-    collector_count_for_album += 1
-
-    if $all_collectors[collector["fan_id"]]
-      $all_collectors[collector["fan_id"]][:tralbum_ids] += [tralbum_id]
-    else
-      $all_collectors[collector["fan_id"]] = {
-        fan_id: collector["fan_id"],
-        tralbum_ids: [tralbum_id],
-        name: collector["name"],
-        username: collector["username"],
-        url: collector["url"],
-      }
-    end
-  end
-
-  more_collectors_available = parsed_initial_response["more_thumbs_available"]
-  last_token = (parsed_initial_response["thumbs"].last || {}) ["token"]
-
-  while more_collectors_available do
-    puts "getting more collectors for album #{tralbum_id}."
-    response = Faraday.post(
-      "https://bandcamp.com/api/tralbumcollectors/2/thumbs",
-      { tralbum_type: tralbum_type, tralbum_id: tralbum_id, token: last_token, count: 500 }.to_json,
+    initial_response = Faraday.post(
+      "https://bandcamp.com/api/tralbumcollectors/2/initial",
+      { tralbum_type: tralbum_type, tralbum_id: tralbum_id, reviews_count: 0, thumbs_count: 500, exclude_fan_ids: [$fan_id] }.to_json,
       "Content-Type" => "application/json"
     )
 
-    parsed_response = JSON.parse(response.body)
+    parsed_initial_response = JSON.parse(initial_response.body)
 
-    parsed_response["results"].each do |collector|
+    parsed_initial_response["thumbs"].each do |collector|
       collector_count_for_album += 1
 
       if $all_collectors[collector["fan_id"]]
@@ -128,15 +107,45 @@ def get_collectors(tralbum_id:, tralbum_type:)
       end
     end
 
-    more_collectors_available = parsed_response["more_available"]
-    last_token = parsed_response["results"].last["token"]
+    more_collectors_available = parsed_initial_response["more_thumbs_available"]
+    last_token = (parsed_initial_response["thumbs"].last || {}) ["token"]
+
+    while more_collectors_available do
+      puts "getting more collectors for album #{tralbum_id}."
+      response = Faraday.post(
+        "https://bandcamp.com/api/tralbumcollectors/2/thumbs",
+        { tralbum_type: tralbum_type, tralbum_id: tralbum_id, token: last_token, count: 500 }.to_json,
+        "Content-Type" => "application/json"
+      )
+
+      parsed_response = JSON.parse(response.body)
+
+      parsed_response["results"].each do |collector|
+        collector_count_for_album += 1
+
+        if $all_collectors[collector["fan_id"]]
+          $all_collectors[collector["fan_id"]][:tralbum_ids] += [tralbum_id]
+        else
+          $all_collectors[collector["fan_id"]] = {
+            fan_id: collector["fan_id"],
+            tralbum_ids: [tralbum_id],
+            name: collector["name"],
+            username: collector["username"],
+            url: collector["url"],
+          }
+        end
+      end
+
+      more_collectors_available = parsed_response["more_available"]
+      last_token = parsed_response["results"].last["token"]
+    end
+
+    puts "#{collector_count_for_album} collectors found for album #{tralbum_id}. Now at #{$all_collectors.keys.count} total collectors."
   end
 
-  puts "#{collector_count_for_album} collectors found for album #{tralbum_id}. Now at #{$all_collectors.keys.count} total collectors."
-end
-
-$all_albums.each do |album|
-  get_collectors(tralbum_id: album[:tralbum_id], tralbum_type: album[:tralbum_type])
+  $all_albums.each do |album|
+    get_collectors(tralbum_id: album[:tralbum_id], tralbum_type: album[:tralbum_type])
+  end
 end
 
 collectors_with_more_than_one_album = $all_collectors.values.select do |collector|
