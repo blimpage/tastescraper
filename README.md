58dfbba9b847b7f371fac91fed24622988015c55
Pat McKenna <pat@mckenna.rocks>
2021-02-14 16:17:49 +1100

    Tidy up readme, remove some commented-out code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
+# Tastescraper
+
+This is intended to be a little Ruby script that you can run locally that'll find albums on Bandcamp that you might like, based on the collections of people who own the same albums as you.
+
+That's the idea, anyway. It's currently a big procedural mess that only does about half of that. Please don't judge me
--- scraper.rb
+++ scraper.rb
@@ -199,15 +199,3 @@ collectors_to_crawl = collectors_by_compatibility
 puts "collectors_to_crawl:"
 puts collectors_to_crawl.map { |collector| collector[:username] }
 
-
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
